d370bb554a72e89ec1ee5c99ea6aec4e7219eb68
florinres <raresbolca@gmail.com>
2025-03-13 13:05:04 +0200

    Add project files.

---
+++ Regasirea Informatiei/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regasirea_Informatiei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectPath = Environment.CurrentDirectory;
+            string resourcesPath = projectPath + "\\Reuters\\Reuters_34\\Training";
+            string[] files = Directory.GetFiles(resourcesPath);
+            printFiles(files);
+            WordProcessor wordProcessor = new WordProcessor();
+            wordProcessor.processFiles(files);
+        }
+
+        private static void printFiles(string[] files)
+        {
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
---
+++ Regasirea Informatiei/Words.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regasirea_Informatiei
+{
+    class Words
+    {
+        private List<string> words;
+        
+        public Words()
+        {
+            words = new List<string>();
+        }
+
+        public void parseText(string text)
+        {
+            foreach (string word in text.Split(' '))
+            {
+                if(words.Contains(word))
+                {
+                    return;
+                }
+                else
+                {
+                    words.Add(word);
+                }
+            }
+        }   
+    }
+}
