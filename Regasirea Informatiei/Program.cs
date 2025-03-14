5df7d03f305eb4b725caa6218e2dabb7e114bfc7
florinres <raresbolca@gmail.com>
2025-03-14 17:30:00 +0200

    wtf git

--- Regasirea Informatiei/Program.cs
+++ Regasirea Informatiei/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
 using WordFrequency;
@@ -11,28 +12,62 @@ namespace Regasirea_Informatiei
     class Program
     {
         public static List<string> uniqueGlobalWords = new List<string>();
-        public static Dictionary<int, Dictionary<int, int>> wordFrequency = new Dictionary<int, Dictionary<int, int>>();
+        public static Dictionary<int, Dictionary<int, int>> wordFrequencyDictionary = new Dictionary<int, Dictionary<int, int>>();
         static void Main(string[] args)
         {
             string projectPath = Environment.CurrentDirectory;
             string resourcesPath = projectPath + "\\Reuters\\Reuters_34\\Training";
             string[] files = Directory.GetFiles(resourcesPath);
-            //printFiles(files);
             readFiles(files);
             printGlobalWord();
+            saveToFile();
+        }
+
+        private static void saveToFile()
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+            string uniqueWordsFilePath = Path.Combine(currentDirectory, "uniqueGlobalWords.txt");
+            string wordFrequencyFilePath = Path.Combine(currentDirectory, "wordFrequencyDictionary.txt");
+
+            // Save uniqueGlobalWords to file
+            try
+            {
+                File.WriteAllLines(uniqueWordsFilePath, uniqueGlobalWords);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error writing to {uniqueWordsFilePath}: {e.Message}");
+            }
+
+            // Save wordFrequencyDictionary to file
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(wordFrequencyFilePath))
+                {
+                    foreach (var fileEntry in wordFrequencyDictionary)
+                    {
+                        int fileKey = fileEntry.Key;
+                        var wordFrequencies = fileEntry.Value;
+                        string line = $"{fileKey}: [{string.Join(", ", wordFrequencies.Select(kvp => $"{kvp.Key}:{kvp.Value}"))}]";
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error writing to {wordFrequencyFilePath}: {e.Message}");
+            }
         }
 
         private static void printGlobalWord()
         {
-            foreach (string word in uniqueGlobalWords)
-            {
-                Console.WriteLine(word);
-            }
+           Console.WriteLine("Unique global words: " + uniqueGlobalWords.Count);
+           Console.WriteLine("Word frequency dictionary size: " + wordFrequencyDictionary.Count);
         }
 
         private static void readFiles(string[] files)
         {
-            //int ctFisier = 0;
+            int fileKey = 0;
             foreach (string file in files)
             {
                 try
@@ -51,7 +86,8 @@ namespace Regasirea_Informatiei
                     {
                         titleWords = node.InnerText;
                     }
-                    verifyIfStopWordAndExtractRoots(textWords, titleWords);
+                    verifyIfStopWordAndExtractRoots(textWords, titleWords, fileKey);
+                    fileKey++;
                 }
                 catch (Exception ex)
                 {
@@ -60,46 +96,129 @@ namespace Regasirea_Informatiei
             }
         }
 
-        private static void verifyIfStopWordAndExtractRoots(string textWords, string titleWords)
+        private static void verifyIfStopWordAndExtractRoots(string textWords, string titleWords, int ctFisier)
         {
-            string[] titleWordsSplitted = textWords.Split(' ');
-            string[] textWordsSplitted = titleWords.Split(' ');
-            List<string> words = new List<string>();
-            string currentSolutionPath = Environment.CurrentDirectory;
-            string fileName = "\\stopwords.txt";
-            string[] stopWords = readStopWords(currentSolutionPath + fileName);
+            string[] titleWordsSplitted = titleWords.Split(' ');
+            string[] textWordsSplitted = textWords.Split(' ');
+            string currentSolutionPath = Environment.CurrentDirectory + "\\stopwords.txt";
+            HashSet<string> stopWords = new HashSet<string>(readStopWords(currentSolutionPath));
 
-            foreach (string word in titleWordsSplitted)
+            PorterStemmer porterStemmer = new PorterStemmer();
+            Regex regex = new Regex(@"[^a-zA-Z0-9]", RegexOptions.Compiled);
+
+            foreach (string word in titleWordsSplitted.Concat(textWordsSplitted))
             {
-                words.Add(word);
-            }
-            foreach (string word in textWordsSplitted)
-            {
-                words.Add(word);
-            }
-            foreach (string word in words)
-            {
+                if (word.Length == 1)
+                {
+                    continue;
+                }
+
+                string cleanedWord = porterStemmer.StemWord(word.ToLower());
+                cleanedWord = regex.Replace(cleanedWord, "");
+                
                 if (stopWords.Contains(word))
                 {
-                    //don't add it
-                    return;
+                    continue;
                 }
-                else
+
+                if (string.IsNullOrEmpty(cleanedWord) || ContainsOnlyNumbers(cleanedWord))
                 {
-                    PorterStemmer porterStemmer = new PorterStemmer();
-                    string cleanedWord = porterStemmer.StemWord(word);
-                    cleanedWord = Regex.Replace(cleanedWord, @"[^a-zA-Z0-9]", "").ToLower();
-                    if (cleanedWord == "" || ContainsOnlyNumbers(cleanedWord)) 
+                    continue;
+                }
+                
+                cleanedWord = removeNumbers(cleanedWord);
+
+                if (!uniqueGlobalWords.Contains(cleanedWord))
+                {
+                    uniqueGlobalWords.Add(cleanedWord);
+                    addToWordFrequencyDictionary(cleanedWord, ctFisier);
+                }
+            }
+        }
+
+        private static string removeNumbers(string cleanedWord)
+        {
+            bool numbersBefore = false;
+            bool numbersAfter = false;
+
+            foreach(char c in cleanedWord)
+            {
+                if(Char.IsDigit(c))
+                {
+                    numbersBefore = true;
+                    break;
+                }
+            }
+            for(char c = cleanedWord.Last(); c >= 0; c--)
+            {
+                if (Char.IsDigit(c))
+                {
+                    numbersAfter = true;
+                    break;
+                }
+            }
+
+            if (numbersBefore)
+            {
+                foreach (char c in cleanedWord)
+                {
+                    if (Char.IsLetter(c))
                     {
-                        //|| ContainsOnlyNumbers(cleanedWord)
-                        continue;
-                    }
-                    if (!uniqueGlobalWords.Contains(cleanedWord))
-                    {
-                        uniqueGlobalWords.Add(cleanedWord);
-                        Console.WriteLine(cleanedWord);
+                        cleanedWord = cleanedWord.Substring(cleanedWord.IndexOf(c));
+                        break;
                     }
                 }
+            }
+
+            if (numbersAfter)
+            {
+                foreach (char c in cleanedWord.Reverse())
+                {
+                    if (Char.IsLetter(c))
+                    {
+                        cleanedWord = cleanedWord.Substring(0, cleanedWord.LastIndexOf(c) + 1);
+                        break;
+                    }
+                }
+            }
+
+            return cleanedWord;
+        }
+
+        private static void addToWordFrequencyDictionary(string cleanedWord, int fileKey)
+        {
+            Dictionary<int, int> valuePairs = new Dictionary<int, int>();
+            //if (!wordFrequencyDictionary.ContainsKey(fileKey))
+            //{
+            //    valuePairs = new Dictionary<int, int>();
+            //    valuePairs.Add(uniqueGlobalWords.IndexOf(cleanedWord), 1);
+            //    wordFrequencyDictionary.Add(fileKey, new Dictionary<int, int>());
+            //    return;
+            //}
+
+            //valuePairs = wordFrequencyDictionary[fileKey];
+            //if (valuePairs.ContainsValue(uniqueGlobalWords.IndexOf(cleanedWord)))
+            //{
+            //    return;
+            //}
+            //else
+            //{
+            //    valuePairs.Add(uniqueGlobalWords.IndexOf(cleanedWord), 1);
+            //}
+            //wordFrequencyDictionary[fileKey] = valuePairs;
+            if (!wordFrequencyDictionary.ContainsKey(fileKey)){
+                wordFrequencyDictionary.Add(fileKey, valuePairs);
+            }
+            valuePairs = wordFrequencyDictionary[fileKey];
+            int globalWordIndex = uniqueGlobalWords.IndexOf(cleanedWord);
+            if(!valuePairs.ContainsValue(globalWordIndex))
+            {
+                valuePairs.Add(globalWordIndex, 1);
+                wordFrequencyDictionary[fileKey] = valuePairs;
+            }
+            else
+            {
+                return;
             }
         }
 
@@ -120,7 +239,7 @@ namespace Regasirea_Informatiei
             try
             {
                 string stopWords = File.ReadAllText(path);
-                return stopWords.Split('\n');
+                return stopWords.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
             }
             catch (IOException e)
             {
